83e4fa602db742c0ee9a80738fb46cd6896bd5bb
Angel Hermozo <mickyhermozo72@gmail.com>
2024-09-09 00:48:37 -0400

    CORS para upload

--- UploadApi/Program.cs
+++ UploadApi/Program.cs
@@ -1,4 +1,3 @@
-
 namespace UploadApi
 {
     public class Program
@@ -6,30 +5,30 @@ namespace UploadApi
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
-
-            // Add services to the container.
-
             builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy("AllowAllOrigins",
+                    policy =>
+                    {
+                        policy.AllowAnyOrigin()   
+                              .AllowAnyHeader()   
+                              .AllowAnyMethod();   
+                    });
+            });
 
             var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
                 app.UseSwagger();
                 app.UseSwaggerUI();
             }
-
             app.UseHttpsRedirection();
-
+            app.UseCors("AllowAllOrigins");
             app.UseAuthorization();
-
-
             app.MapControllers();
-
             app.Run();
         }
     }
