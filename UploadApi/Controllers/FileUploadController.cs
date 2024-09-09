f5d1f159c2c0bd7a3e82da6e643d3bc5ac5c72e3
Angel Hermozo <mickyhermozo72@gmail.com>
2024-09-08 22:21:35 -0400

    upload de imagnes

---
+++ UploadApi/Controllers/FileUploadController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UploadApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FileUploadController : ControllerBase
+    {
+        [HttpPost("upload")]
+        public async Task<IActionResult> UploadFiles([FromForm] IEnumerable<IFormFile> files)
+        {
+            if (files == null || !files.Any())
+            {
+                return BadRequest("No files uploaded.");
+            }
+
+            var uploadsFolder = Path.Combine(Directory.GetCurrentDirectory(), "UploadedFiles");
+            if (!Directory.Exists(uploadsFolder))
+            {
+                Directory.CreateDirectory(uploadsFolder);
+            }
+
+            var filePaths = new List<string>();
+
+            foreach (var file in files)
+            {
+                if (file.Length > 0)
+                {
+                    var filePath = Path.Combine(uploadsFolder, file.FileName);
+
+                    using (var stream = new FileStream(filePath, FileMode.Create))
+                    {
+                        await file.CopyToAsync(stream);
+                    }
+
+                    filePaths.Add(filePath);
+                }
+            }
+
+            return Ok(new { filePaths });
+        }
+    }
+}
